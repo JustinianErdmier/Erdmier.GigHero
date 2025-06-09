b49a23ef44ec0f343401406ee9a452db7c522cdd
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-09 15:03:04 -0500

    feat(🏷️): add types
    Expanded on the request models by adding a response model as well.

---
+++ Source/Client.Shared/Common/Interfaces/IRequestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Erdmier.GigHero.Client.Shared.Common.Interfaces;
+
+public interface IRequestResponse
+{
+    string? ErrorMessage { get; }
+
+    bool IsSuccess { get; }
+
+    bool IsFailure => !IsSuccess;
+}
+
+public interface IRequestResponse<out TReturnData> : IRequestResponse
+{
+    TReturnData? Data { get; }
+
+    [ MemberNotNullWhen(returnValue: true, nameof(Data)) ]
+    bool HasData { get; }
+}
---
+++ Source/Client.Shared/Gigs/Models/Requests/CreateGigResponse.cs
@@ -0,0 +1,13 @@
+﻿using Erdmier.GigHero.Client.Shared.Common.Interfaces;
+using Erdmier.GigHero.Client.Shared.Gigs.Models.DTOs;
+
+namespace Erdmier.GigHero.Client.Shared.Gigs.Models.Requests;
+
+public sealed record CreateGigResponse(string? ErrorMessage, bool IsSuccess, GigDto? Data) : IRequestResponse<GigDto>
+{
+    public bool HasData => Data is not null;
+
+    public static CreateGigResponse Success(GigDto? data) => new(ErrorMessage: null, IsSuccess: true, data);
+
+    public static CreateGigResponse Failure(string? errorMessage) => new(errorMessage, IsSuccess: false, Data: null);
+}
